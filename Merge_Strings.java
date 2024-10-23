6a1aeede37224ea0eb520b17e58c4ef4ae0cf9f5
nitinsingh33 <nitinsingh.iitp@gmail.com>
2024-10-23 18:33:36 +0530

    .

--- Merge_Strings.java
+++ Merge_Strings.java
@@ -10,3 +10,22 @@ word2:    p   q   r
 merged: a p b q c r
 
 */
+
+public class Merge_Strings {
+
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        while (i < word1.length() || i < word2.length()) {
+            if (i < word1.length()) {
+                result.append(word1.charAt(i));
+            }
+            if (i < word2.length()) {
+                result.append(word2.charAt(i));
+            }
+            i++;
+        }
+        return result.toString(); 
+    }
+    
+}
